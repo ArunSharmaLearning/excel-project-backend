43d2928331e4536e22229a561b4f728c7033b14a
arunsharmalearning <arunlivingfantastico@gmail.com>
2024-09-14 23:27:20 +0530

    fix: download bug

--- routes/download.js
+++ routes/download.js
@@ -4,6 +4,7 @@ const { processFile } = require("../controller/apply-filter");
 
 router.post("/:name", async (req, res) => {
   console.log("PARAM", req.params, req.body)
+  const filters = req.body
   processFile(path.join(process.env.BASE_API_URL, 'uploads', req.params.name), filters)
   res.status(200).json({ path: path.join(process.env.BASE_API_URL, 'uploads', 'output.xlsx') });
 });
